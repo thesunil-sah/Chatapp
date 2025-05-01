a577ea577ce03965585597fedf5a47278ab3eacf
sahs11 <sahs11@coventry.ac.uk>
2025-05-02 00:53:12 +0100

    adding HuggingFaceHub with flan-t5-xxl for local llm

--- src/llm.py
+++ src/llm.py
@@ -1,14 +1,18 @@
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
+from langchain.llms import HuggingFaceHub
 from dotenv import load_dotenv
 
 load_dotenv()
 def get_conversation_chain(retriever):
-    llm = ChatOpenAI(
-        temperature =0,
-        model_name="gpt-3.5-turbo"
-                     )
+    # llm = ChatOpenAI(
+    #     temperature =0,
+    #     model_name="gpt-3.5-turbo"
+    #                  )
+    
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
 
     memory = ConversationBufferMemory(
         memory_key="chat_history",return_messages=True
