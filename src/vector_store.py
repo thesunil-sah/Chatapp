85fe461784547096a2cfbe2328011d553b00705f
sahs11 <sahs11@coventry.ac.uk>
2025-05-01 23:29:23 +0100

    Adding Hugging Face embeddings with BAAI/bge-base-en-v1.5

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,12 +1,13 @@
 from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceBgeEmbeddings
 from langchain.schema import Document
 from dotenv import load_dotenv
 
 load_dotenv() # load all api key 
 
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
+    #embeddings = OpenAIEmbeddings()
+    embeddings =HuggingFaceBgeEmbeddings(model_name="BAAI/bge-base-en-v1.5")
     vectorstore = FAISS.from_documents(text_chunks,embedding=embeddings)
 
     return vectorstore
